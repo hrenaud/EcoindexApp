3a45146238aff4230b83078ad751de9ce520a63b
Renaud Heluin <renaud.heluin@novagaia.fr>
2025-12-06 11:06:39 +0100

    chore: add Prettier for code formatting and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,98 +1,109 @@
-import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import globals from 'globals';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
 
 export default [
-  js.configs.recommended,
-  {
-    ignores: ['lib/**', 'out/**', '.vite/**', 'dist/**', 'dist-electron/**', 'node_modules/**'],
-  },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        process: 'readonly',
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+    js.configs.recommended,
+    prettier,
+    {
+        ignores: [
+            'lib/**',
+            'out/**',
+            '.vite/**',
+            'dist/**',
+            'dist-electron/**',
+            'node_modules/**',
+        ],
     },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                process: 'readonly',
+            },
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+        },
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                process: 'readonly',
+            },
+            parser: tsparser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            '@typescript-eslint': tseslint,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_' },
+            ],
+            'no-unused-vars': 'off',
+        },
     },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
+    {
+        files: ['forge.config.js', 'vite.*.config.ts'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                process: 'readonly',
+                __dirname: 'readonly',
+            },
+            parser: tsparser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
     },
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        process: 'readonly',
-      },
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+    {
+        ignores: ['dist/**', '.vite/**', 'out/**', 'node_modules/**', '*.d.ts'],
     },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      '@typescript-eslint': tseslint,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'no-unused-vars': 'off',
-    },
-  },
-  {
-    files: ['forge.config.js', 'vite.*.config.ts'],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        process: 'readonly',
-        __dirname: 'readonly',
-      },
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-  },
-  {
-    ignores: ['dist/**', '.vite/**', 'out/**', 'node_modules/**', '*.d.ts'],
-  },
-];
-
+]
