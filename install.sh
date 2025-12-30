e016e4d4f6e65fa89d9f03d3bce61b540c0165ec
Renaud Heluin <renaud.heluin@novagaia.fr>
2025-12-30 12:21:20 +0100

    feat: ajouter étape premake dans install.sh
    - Exécuter npm run premake après l'installation des dépendances
    - Permet de packer lib.asar avant l'installation des dépendances de lib

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 echo "Installing dependencies..."
 npm ci
 echo "Dependencies installed"
+echo "Running premake..."
+npm run "premake"
+echo "Premake completed"
 echo "Installing lib dependencies..."
 cd lib && npm ci
 echo "Lib dependencies installed"
